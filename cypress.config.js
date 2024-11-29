dd441fc71204b08e7cbb84e756e388582391752a
Rodrigo Llull <rodrigollullrodriguez@gmail.com>
2024-11-29 10:18:45 -0300

    Fix env credentials in gitHub

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://d2yqnm7qbjnp0v.cloudfront.net",
+    baseUrl: process.env.baseUrl || "https://d2yqnm7qbjnp0v.cloudfront.net",
     env: {
-      email: "",
-      password: "",
+      email: process.env.email || "",
+      password: process.env.password || "",
     },
     defaultCommandTimeout: 15000
   },
